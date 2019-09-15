aa45556f6818da46ed8325c65bfac76f9d47147c
Levin Mauritz <l.mauritz@posteo.de>
2019-09-15 14:12:37 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => (
+    <div>
+        <h1>We have lift off! 🚀</h1>
+    </div>
+);
+
+export { App }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+
+const render = () => ReactDOM.render(<App/>, document.getElementById('root'))
+document.addEventListener('DOMContentLoaded', render)
